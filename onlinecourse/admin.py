29afbbd35c9e34905b2dc19485e99841c00bacf7
Valentyn Hrabovskyi <valentin.grabovskiy@gmail.com>
2022-05-20 08:27:56 +0200

    Extend admin site

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
@@ -10,9 +9,13 @@ class LessonInline(admin.StackedInline):
     extra = 5
 
 
+class QuestionInline(admin.StackedInline):
+    model = Question
+
+
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
@@ -22,9 +25,17 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
-# <HINT> Register Question and Choice models here
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
